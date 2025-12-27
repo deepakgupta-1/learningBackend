08859e83db13eff2da763e28507ae9e6fbc95bbd
deepakgupta-1 <deepakgupta902765@gmail.com>
2025-12-27 18:39:13 +0530

    some middlewares

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import dotenv from "dotenv";
-dotenv.config();
+import {app} from "./app.js";
 import connectDB from "./db/index.js";
 
+dotenv.config();
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })  
+})
+.catch((err) => {
+  console.error("Failed to connect to the database:", err);
+});
 
